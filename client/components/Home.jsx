989f960e1d8de1867797c39b9ba80700ea1c9009
cyng24 <cyng24@gmail.com>
2018-02-03 17:34:40 -0500

    added back files

---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Home extends Component {
+
+  render () {
+    return (
+      <home>
+        <h1>Fullstack Programmer / Designer</h1>
+      </home>
+    );
+  }
+}
---
+++ client/components/Resume.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Resume extends Component {
+
+  render () {
+    return (
+      <resume>
+        <img className="resume-image" src="images/resume.png" />
+      </resume>
+    );
+  }
+}
