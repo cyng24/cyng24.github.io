4ca67c9264f60153dda4b37a1c2583dc4f5bd8b5
cyng24 <cyng24@gmail.com>
2018-02-13 23:42:23 -0500

    updated project page

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -3,70 +3,193 @@ import { Link } from 'react-router-dom';
 
 export default class Projects extends Component {
 
-  render () {
+  coquillClick() { 
+    var myImg = document.getElementsByClassName('coquill');
+    for (var i=0; i<myImg.length; i++) {
+      var displaySetting = myImg[i].style.opacity;
+      if (displaySetting == "0") { myImg[i].style.opacity = 0.9; }
+      if (displaySetting == "1") { myImg[i].style.opacity = 0.2; }
+      if (displaySetting == "0.9") { myImg[i].style.opacity = 0; }
+      if (displaySetting == "0.2") { myImg[i].style.opacity = 1; }
+    }
+    var otherImg = [document.getElementsByClassName('meltdown')];
+    otherImg.push(document.getElementsByClassName('deep'));
+    otherImg.push(document.getElementsByClassName('butterfly'));
+    otherImg.push(document.getElementsByClassName('award'));
+    for (var i=0; i<otherImg.length; i++) {
+      for (var j=0; j<otherImg[i].length; j++) {
+        var displaySetting = otherImg[i][j].style.opacity;
+        if (displaySetting == "0.9") { otherImg[i][j].style.opacity = 0; }
+        if (displaySetting == "0.2") { otherImg[i][j].style.opacity = 1; }
+      }
+    }
+  }
 
+  meltdownClick() { 
+    var myImg = document.getElementsByClassName('meltdown');
+    for (var i=0; i<myImg.length; i++) {
+      var displaySetting = myImg[i].style.opacity;
+      if (displaySetting == "0") { myImg[i].style.opacity = 0.9; }
+      if (displaySetting == "1") { myImg[i].style.opacity = 0.2; }
+      if (displaySetting == "0.9") { myImg[i].style.opacity = 0; }
+      if (displaySetting == "0.2") { myImg[i].style.opacity = 1; }
+    }
+    var otherImg = [document.getElementsByClassName('coquill')];
+    otherImg.push(document.getElementsByClassName('deep'));
+    otherImg.push(document.getElementsByClassName('butterfly'));
+    otherImg.push(document.getElementsByClassName('award'));
+    for (var i=0; i<otherImg.length; i++) {
+      for (var j=0; j<otherImg[i].length; j++) {
+        var displaySetting = otherImg[i][j].style.opacity;
+        if (displaySetting == "0.9") { otherImg[i][j].style.opacity = 0; }
+        if (displaySetting == "0.2") { otherImg[i][j].style.opacity = 1; }
+      }
+    }
+  }
+
+  deepClick() { 
+    var myImg = document.getElementsByClassName('deep');
+    for (var i=0; i<myImg.length; i++) {
+      var displaySetting = myImg[i].style.opacity;
+      if (displaySetting == "0") { myImg[i].style.opacity = 0.9; }
+      if (displaySetting == "1") { myImg[i].style.opacity = 0.2; }
+      if (displaySetting == "0.9") { myImg[i].style.opacity = 0; }
+      if (displaySetting == "0.2") { myImg[i].style.opacity = 1; }
+    }
+    var otherImg = [document.getElementsByClassName('coquill')];
+    otherImg.push(document.getElementsByClassName('meltdown'));
+    otherImg.push(document.getElementsByClassName('butterfly'));
+    otherImg.push(document.getElementsByClassName('award'));
+    for (var i=0; i<otherImg.length; i++) {
+      for (var j=0; j<otherImg[i].length; j++) {
+        var displaySetting = otherImg[i][j].style.opacity;
+        if (displaySetting == "0.9") { otherImg[i][j].style.opacity = 0; }
+        if (displaySetting == "0.2") { otherImg[i][j].style.opacity = 1; }
+      }
+    }
+  }
+
+  butterflyClick() { 
+    var myImg = document.getElementsByClassName('butterfly');
+    for (var i=0; i<myImg.length; i++) {
+      var displaySetting = myImg[i].style.opacity;
+      if (displaySetting == "0") { myImg[i].style.opacity = 0.9; }
+      if (displaySetting == "1") { myImg[i].style.opacity = 0.2; }
+      if (displaySetting == "0.9") { myImg[i].style.opacity = 0; }
+      if (displaySetting == "0.2") { myImg[i].style.opacity = 1; }
+    }
+    var otherImg = [document.getElementsByClassName('coquill')];
+    otherImg.push(document.getElementsByClassName('meltdown'));
+    otherImg.push(document.getElementsByClassName('deep'));
+    otherImg.push(document.getElementsByClassName('award'));
+    for (var i=0; i<otherImg.length; i++) {
+      for (var j=0; j<otherImg[i].length; j++) {
+        var displaySetting = otherImg[i][j].style.opacity;
+        if (displaySetting == "0.9") { otherImg[i][j].style.opacity = 0; }
+        if (displaySetting == "0.2") { otherImg[i][j].style.opacity = 1; }
+      }
+    }
+  }
+
+  awardClick() { 
+    var myImg = document.getElementsByClassName('award');
+    for (var i=0; i<myImg.length; i++) {
+      var displaySetting = myImg[i].style.opacity;
+      if (displaySetting == "0") { myImg[i].style.opacity = 0.9; }
+      if (displaySetting == "1") { myImg[i].style.opacity = 0.2; }
+      if (displaySetting == "0.9") { myImg[i].style.opacity = 0; }
+      if (displaySetting == "0.2") { myImg[i].style.opacity = 1; }
+    }
+    var otherImg = [document.getElementsByClassName('coquill')];
+    otherImg.push(document.getElementsByClassName('meltdown'));
+    otherImg.push(document.getElementsByClassName('deep'));
+    otherImg.push(document.getElementsByClassName('butterfly'));
+    for (var i=0; i<otherImg.length; i++) {
+      for (var j=0; j<otherImg[i].length; j++) {
+        var displaySetting = otherImg[i][j].style.opacity;
+        if (displaySetting == "0.9") { otherImg[i][j].style.opacity = 0; }
+        if (displaySetting == "0.2") { otherImg[i][j].style.opacity = 1; }
+      }
+    }
+  }
+
+  render () {
     return (
       <projects>
-      {/* <h1>Projects</h1> */}
-      {/* <img className="projects-arrow" src="images/down_arrow.png" /> */}
         <section id="project-list">
           <div id="flex">
             <p>
-              <a className="project-title" href="https://coquill-e559a.firebaseapp.com/"><img className="project-image" src="images/burnt_quill.png" /></a>  
-              <text className="project-text">
-              <br /><br />
-              <b>COQUILL</b><br />
-              <b>_______</b><br />
-              A web-based tool for screenwriters to collaborate in real time on a document that automatically formats into a screenplay.
+              <input type="image" class="project-image coquill" src="images/burnt_quill.png" style={{ opacity: 1 }} onClick={ this.coquillClick } />
+              <text class="project-name coquill" style={{ opacity: 0 }}>
+                <b>COQUILL</b><br />
+                <b>_______</b>
               </text>
             </p>
 
             <p>
-              <a className="project-title" href="https://thefinalmeltdown.glitch.me"><img className="project-image" src="images/finalmeltdown.png" /></a>
-              <text className="project-text">
-              <br /><br />
-              <b>FINAL MELTDOWN</b><br />
-              <b>______</b><br />
-              A virtual reality app that depicts the effects of climate change by placing the viewer in a melting icescape.
+              <input type="image" className="project-image meltdown" src="images/finalmeltdown.png" style={{ opacity: 1 }} onClick={ this.meltdownClick } />
+              <text className="project-name meltdown" style={{ opacity: 0 }}>
+                <b>FINAL MELTDOWN</b><br />
+                <b>_______</b>
+              </text>
+            </p>
+
+             <p>
+              <input type="image" className="project-image deep" src="images/whale.png" style={{ opacity: 1 }} onClick={ this.deepClick } />
+              <text className="project-name deep" style={{ opacity: 0 }}>
+                <b>HACK THE DEEP</b><br />
+                <b>_______</b>
               </text>
             </p>
             
             <p>
-              <a className="project-title" href="https://youtube.com/watch?v=Y9MLgQTFiHk"><img className="project-image" src="images/butterfly.png" /></a>  
-              <text className="project-text">
-              <br /><br />
-              <b>BUTTERFLY HUNT</b><br />
-              <b>_______</b><br />
-              A mobile augmented reality game where Grace Hopper students search for butterflies that appear on the the GH logo.
+              <input type="image" className="project-image butterfly" src="images/butterfly.png" style={{ opacity: 1 }} onClick={ this.butterflyClick } /> 
+              <text className="project-name butterfly" style={{ opacity: 0 }}>
+                <b>BUTTERFLY HUNT</b><br />
+                <b>_______</b>
               </text>
             </p>
 
             <p>
-              <a className="project-title"><img className="project-image" src="images/award.png" /></a>
-              <text className="project-text">
-                <br /><br />
+              <input type="image" className="project-image award" src="images/award.png" style={{ opacity: 1 }} onClick={ this.awardClick } />
+              <text className="project-name award" style={{ opacity: 0 }}>
                 <b>AWARDS</b><br />
-                <b>_______</b><br />
-                <b>Red Hat Best Open Source Project</b>
-                <br />
-                <i>SheHacks Boston</i>
+                <b>_______</b>
               </text>
             </p>
           </div>
-        </section>
-{/*         
-        <h1>Awards</h1>
-          <img id="awards-arrow" alt="image_not" src="images/down_arrow.png" />
-          <section id="awards">
-            <p>
-              <text className="carousel-text">
-              <b>Red Hat Best Open Source Project</b>
-              <br></br>
-              <i>SheHacks Boston</i>
-              </text>
+
+          <div>
+            <p class="project-text coquill" style={{ opacity: 0 }}>
+              A web-based tool for screenwriters to collaborate in real time on a document that automatically formats into a screenplay.<br /><br />
+              <b style={{ fontSize: 12 }}>Javascript CSS HTML Firebase Immutable</b><br /><br />
+              <a href="https://coquill-e559a.firebaseapp.com/"><button class="button">try me</button></a>
             </p>
-          </section>
-*/}
+
+            <p class="project-text meltdown" style={{ opacity: 0 }}>
+              A virtual reality app that depicts the effects of climate change by placing the viewer in a melting icescape.<br /><br />
+              <b style={{ fontSize: 12 }}>Javascript HTML A-Frame Glitch Samsung Gear VR</b><br /><br />
+              <a href="https://thefinalmeltdown.glitch.me/"><button class="button">try me</button></a>
+            </p>
+
+            <p class="project-text deep" style={{ opacity: 0 }}>
+              A data visualization of ocean currents to plot and predict the most likely path of plastics that end up in the North Pacific Garbage Patch.<br /><br />
+              <b style={{ fontSize: 12 }}>HTML Python Pandas CartoJS</b><br /><br />
+              <a href="https://hackthedeep.github.io/flotsam-and-jetsam"><button class="button">try me</button></a>
+            </p>
+
+            <p class="project-text butterfly" style={{ opacity: 0 }}>
+              A mobile augmented reality game where Grace Hopper students search for butterflies that appear on the the GH logo.<br /><br />
+              <b style={{ fontSize: 12 }}>Unity C# Vuforia Android Studio</b><br /><br />
+              <a href="https://www.youtube.com/watch?v=Y9MLgQTFiHk"><button class="button">try me</button></a>
+            </p>
+            
+            <p class="project-text award" style={{ opacity: 0 }}>
+              <b>Red Hat Best Open Source Project</b><br />
+              <i>SheHacks Boston</i>
+            </p> 
+          </div>
+        </section>
       </projects>
     );
   }
