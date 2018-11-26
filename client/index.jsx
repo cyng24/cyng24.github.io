e3bfbbc6b4f4ed52e3f6f0cb91820bbda1a67953
cyng24 <cyng24@gmail.com>
2018-11-26 10:12:30 +0100

    actually updated resume and cleaned out dependencies

--- client/index.jsx
+++ client/index.jsx
@@ -2,13 +2,10 @@
 
 import React from 'react'
 import {render} from 'react-dom'
-import { Provider } from 'react-redux'
 
 import Main from './components/Main.jsx'
 
 render (
-  <Provider>
-    <Main />
-  </Provider>,
+    <Main />,
   document.getElementById('main')
 )
